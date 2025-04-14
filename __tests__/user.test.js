0747576966889a9e1727c5affb4a4c10fa4b977d
Stelios Fridakis <stelios.fridakis@hotmail.com>
2025-04-14 21:06:58 +0300

    automated testing

--- __tests__/user.test.js
+++ __tests__/user.test.js
@@ -1,7 +1,8 @@
 const mongoose = require("mongoose");
 const request = require("supertest");
 
-const authService = require('../services/auth.service')
+const authService = require('../services/auth.service');
+const userService = require('../services/user.services');
 
 const app = require('../app');
 // require('dotenv').config();
@@ -55,13 +56,17 @@ describe("Requests for /api/users/:username", () => {
 });
 
 it("GET - Returns specific user", async()=>{
+
+    const result = await userService.findLastInsertedUser();
+    console.log("RESULT>>>", result);
+
     const res = await request(app)
-    .get('/api/users/user1')
+    .get('/api/users/user5')
     .set('Authorization', `Bearer ${token}`);
 
     expect(res.statusCode).toBe(200);
     expect(res.body.status).toBeTruthy();
-    expect(res.body.data.username).toBe('user1');
-    expect(res.body.data.email).toBe('user1@aueb.gr');
+    expect(res.body.data.username).toBe(result.username);
+    expect(res.body.data.email).toBe(result.email);
 })
 });
--- services/user.services.js
+++ services/user.services.js
@@ -10,4 +10,18 @@ function findOne(username) {
     return result;
 }
 
-module.exports = { findAll, findOne };
+async function findLastInsertedUser() {
+    console.log("Find last inserted user.");
+
+    try {
+        const result = await User.find().sort({_id:-1}).limit(1); // sort από τον πιο πρόσφατο στον πιο παλιό, μόνο το πρώτο result
+        // const result = User.findOne()sort.({_id:-1}) // η findOne επιστρέφει ένα document
+        console.log("Success in finding last inserted user.", result[0].username);  // η find επιστρέφει array
+        return result[0];
+    } catch (err) {
+        console.log("Problem in finding last inserted user.", err);
+        return false;
+    }
+}
+
+module.exports = { findAll, findOne, findLastInsertedUser };
